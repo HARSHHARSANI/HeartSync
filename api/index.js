a01b1eb376a16fc28b86d497ab8b20d0a6907b43
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-05-24 10:04:06 +0530

    code

--- api/index.js
+++ api/index.js
@@ -31,16 +31,12 @@ app.post('/register', async (req, res) => {
   try {
     const {userData} = req.body;
 
-    console.log(userData, 'userData');
-
     // Transform the date fields to a Date object
     const dateOfBirth = new Date(
       userData.year,
       userData.month - 1,
       userData.day,
     );
-
-    console.log(dateOfBirth, 'dateOfBirth');
 
     // Transform the incoming data to fit the schema
     const newUser = new userModel({
@@ -51,7 +47,7 @@ app.post('/register', async (req, res) => {
       gender: userData.gender,
       location: userData.location,
       type: userData.type,
-      datingType: userData.datingType,
+      datingType: userData.DatingType,
       lookingFor: userData.lookingFor,
       homeTown: userData.homeTown,
       imageUrls: userData.imgUrls.filter(url => url),
@@ -74,6 +70,186 @@ app.post('/register', async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({message: 'Server error'});
+  }
+});
+
+app.get('/users/:userId', async (req, res) => {
+  try {
+    const {userId} = req.params;
+    const user = await userModel.findById(userId);
+    if (!user) return res.status(404).json({message: 'User not found'});
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Error in fetching user'});
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const user = await userModel
+      .findOne({email, password})
+      .select('_id')
+      .lean();
+
+    if (!user) return res.status(404).json({message: 'Invalid credentials'});
+
+    const secretKey = crypto.randomBytes(32).toString('hex');
+
+    const token = jwt.sign({id: user._id}, secretKey);
+
+    res.status(200).json({message: 'Login successful', token});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server error'});
+  }
+});
+
+//matches
+app.get('/matches', async (req, res) => {
+  try {
+    const {userId} = req.query;
+    const user = await userModel.findById(userId);
+    if (!user)
+      return res.status(404).json({
+        message: 'User not found',
+      });
+
+    let filter = {};
+
+    if (user.gender === 'Men') {
+      filter.gender = 'Women';
+    } else if (user.gender === 'Women') {
+      filter.gender = 'Men';
+    }
+
+    let query = {
+      _id: {$in: user.matches},
+    };
+
+    if (user.type) {
+      filter.type = user.type;
+    }
+
+    const currentUser = await userModel
+      .findById(userId)
+      .populate('matches', '_id')
+      .populate('likedProfiles', '_id');
+
+    const friends = currentUser.matches.map(friend => friend._id);
+
+    const crushIds = currentUser.likedProfiles.map(crush => crush._id);
+
+    console.log('filter', filter);
+
+    //fetch Matches based on filter
+
+    const matches = await userModel
+      .find()
+      .where('_id')
+      .nin([userId, ...friends, ...crushIds]);
+
+    return res.status(200).json({matches});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server error'});
+  }
+});
+
+app.post('/like-profile', async (req, res) => {
+  try {
+    const {userId, likedUserId, image, comment} = req.body;
+
+    await userModel.findByIdAndUpdate(likedUserId, {
+      $push: {receivedLikes: {userId, image, comment}},
+    });
+
+    await userModel.findByIdAndUpdate(userId, {
+      $push: {likedProfiles: likedUserId},
+    });
+
+    res.status(200).json({message: 'Liked profile'});
+  } catch (error) {
+    console.error('Error liking profile:', error);
+    res.status(500).json({message: 'Internal server error'});
+  }
+});
+
+app.get('/received-likes/:userId', async (req, res) => {
+  try {
+    const {userId} = req.params;
+
+    const likes = await User.findById(userId)
+      .populate('receivedLikes.userId', 'firstName imageUrls prompts')
+      .select('receivedLikes');
+
+    res.status(200).json({receivedLikes: likes.receivedLikes});
+  } catch (error) {
+    console.error('Error fetching received likes:', error);
+    res.status(500).json({message: 'Internal server error'});
+  }
+});
+
+//endpoint to create a match betweeen two people
+app.post('/create-match', async (req, res) => {
+  try {
+    const {currentUserId, selectedUserId} = req.body;
+
+    //update the selected user's crushes array and the matches array
+    await User.findByIdAndUpdate(selectedUserId, {
+      $push: {matches: currentUserId},
+      $pull: {likedProfiles: currentUserId},
+    });
+
+    //update the current user's matches array recievedlikes array
+    await User.findByIdAndUpdate(currentUserId, {
+      $push: {matches: selectedUserId},
+    });
+
+    // Find the user document by ID and update the receivedLikes array
+    const updatedUser = await User.findByIdAndUpdate(
+      currentUserId,
+      {
+        $pull: {receivedLikes: {userId: selectedUserId}},
+      },
+      {new: true},
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({message: 'User not found'});
+    }
+
+    // If the user document was successfully updated
+    res.status(200).json({message: 'ReceivedLikes updated successfully'});
+  } catch (error) {
+    res.status(500).json({message: 'Error creating a match', error});
+  }
+});
+
+// Endpoint to get all matches of a specific user
+app.get('/get-matches/:userId', async (req, res) => {
+  try {
+    const {userId} = req.params;
+
+    // Find the user by ID and populate the matches field
+    const user = await User.findById(userId).populate(
+      'matches',
+      'firstName imageUrls',
+    );
+
+    if (!user) {
+      return res.status(404).json({message: 'User not found'});
+    }
+
+    // Extract matches from the user object
+    const matches = user.matches;
+
+    res.status(200).json({matches});
+  } catch (error) {
+    console.error('Error getting matches:', error);
+    res.status(500).json({message: 'Internal server error'});
   }
 });
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
+  useEffect(() => {
+    showToken();
+  }, []);
+
+  const showToken = async () => {
+    console.log('Token: ', await AsyncStorage.getItem('token'));
+  };
+
   return (
     <View>
       <Text>HomeScreen</Text>
